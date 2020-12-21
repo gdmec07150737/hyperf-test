8b2fa3a90807f56ad2c88d2ee7f54b695a28fa61
gdmec07150737 <1354105797@qq.com>
2020-12-21 18:56:22 +0800

    处理跨域问题

--- config/autoload/middlewares.php
+++ config/autoload/middlewares.php
@@ -10,11 +10,13 @@ declare(strict_types=1);
  * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
  */
 
+use App\Middleware\CorsMiddleware;
 use Hyperf\Session\Middleware\SessionMiddleware;
 use Hyperf\Validation\Middleware\ValidationMiddleware;
 
 return [
     'http' => [
+        CorsMiddleware::class,
         SessionMiddleware::class,
         ValidationMiddleware::class,
     ],
