45b6df35f268cdac68881ef9cfda9d4fd6b09d27
gdmec07150737 <1354105797@qq.com>
2020-12-23 10:47:26 +0800

    修改编辑器提示

--- app/Model/User.php
+++ app/Model/User.php
@@ -10,8 +10,8 @@ use Carbon\Carbon;
  * @property string $password 密码
  * @property string $state 用户状态（normal/正常、abnormal/异常）
  * @property string $salt 加密盐
- * @property \Carbon\Carbon $created_at 用户账号注册时间
- * @property \Carbon\Carbon $updated_at 用户账号修改时间
+ * @property Carbon $created_at 用户账号注册时间
+ * @property Carbon $updated_at 用户账号修改时间
  * @property string $token 用户token
  */
 class User extends Model
